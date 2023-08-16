30f7d5e71873520101c4bfbcd06be20cb79d3356
dev-talib <mohdtalib.dev@gmail.com>
2023-08-16 11:49:27 +0530

    Integrate Redux Persist for state persistence

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,24 @@
 import { createStore, applyMiddleware, compose } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // Use local storage as the default storage engine
 import thunk from 'redux-thunk';
 import rootReducer from '../redux/reducers/rootReducers';
+
+// Configuration for Redux Persist
+const persistConfig = {
+  key: 'root', // key for the root of the storage
+  storage, // storage engine to use
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
-  rootReducer,
+  persistedReducer, // Use the persisted reducer
   composeEnhancers(applyMiddleware(thunk))
 );
 
-export default store;
+const persistor = persistStore(store); // Create a persistor object
+
+export { store, persistor }; // Export both store and persistor
